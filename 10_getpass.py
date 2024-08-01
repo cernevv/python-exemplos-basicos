63a8df4c90e3ae254190c739759e98c217d9df94
Laura Cernev <laura.cernev24@gmail.com>
2024-08-01 16:06:59 -0300

    up

---
+++ 10_getpass.py
@@ -0,0 +1,11 @@
+# Importação da biblioteca getpass
+import getpass as gtp
+
+usuario = input("Nome do usuário: ")
+senha = gtp.getpass("Senha do usuário: ")
+
+# Verificação do número de caracteres da senha
+if len(senha) >= 6:
+    print(f"Usuário cadastrado com sucesso!")
+else:
+    print("Atenção! A senha deve ter no mínimo 6 dígitos!!!")
---
+++ 13_operador_or_1.py
@@ -0,0 +1,9 @@
+idade_kaue = 17
+idade_matheus = 15
+
+# Operador OR (Ou)
+if idade_kaue >=18 or idade_matheus >=18:
+    print("Pelo menos um dos dois é maior de idade.")
+else:
+    # Os 2 devem ser menores para atender esta condição
+    print("Kauê e Matheus não são maiores de idade.")
